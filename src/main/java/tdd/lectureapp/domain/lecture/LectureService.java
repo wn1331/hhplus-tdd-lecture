fc92da22f687f58c2da0522802d4dd0a28dc72e8
wn1331 <wn1331@gmail.com>
2024-10-02 08:07:59 +0900

    chore : LectureService 코드 정리

--- src/main/java/tdd/lectureapp/domain/lecture/LectureService.java
+++ src/main/java/tdd/lectureapp/domain/lecture/LectureService.java
@@ -1,22 +1,18 @@
 package tdd.lectureapp.domain.lecture;
 
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tdd.lectureapp.global.CustomGlobalException;
 import tdd.lectureapp.global.ErrorCode;
-import tdd.lectureapp.domain.enrollment.EnrollmentRepository;
 import tdd.lectureapp.infra.lecture.Lecture;
-import tdd.lectureapp.interfaces.api.dto.LectureApplyDto;
 
 @Service
 @RequiredArgsConstructor
 public class LectureService {
 
     private final LectureRepository lectureRepository;
-    private final EnrollmentRepository enrollmentRepository;
 
     @Transactional
     public Lecture findLecture(LectureCommand command){
@@ -26,10 +22,6 @@ public class LectureService {
                 ErrorCode.LECTURE_NOT_EXIST));
     }
 
-    @Transactional(readOnly = true)
-    public List<String> findAllLectures(){
-        return lectureRepository.findAll().stream().map(Lecture::getLecturer).toList();
-    }
 
 
 
