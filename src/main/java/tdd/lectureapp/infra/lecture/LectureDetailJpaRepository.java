f1a35538a844e74107d6e69d6faf06875f4f7740
wn1331 <wn1331@gmail.com>
2024-10-02 09:16:00 +0900

    chore : PESSIMISTIC_WRITE(비관적 잠금) - 동시성 대응 방안 추가

--- src/main/java/tdd/lectureapp/infra/lecture/LectureDetailJpaRepository.java
+++ src/main/java/tdd/lectureapp/infra/lecture/LectureDetailJpaRepository.java
@@ -1,12 +1,15 @@
 package tdd.lectureapp.infra.lecture;
 
+import jakarta.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface LectureDetailJpaRepository extends JpaRepository<LectureDetail,Long> {
 
     //[STEP3] 동시성 대응 PESSIMISTICLOCK
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<LectureDetail> findByIdAndLectureId(Long id, Long lectureId);
 
     List<LectureDetail> findByCapacityGreaterThanEqual(Long capacity);
